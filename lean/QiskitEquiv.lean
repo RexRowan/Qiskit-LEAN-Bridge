4028a28ca1ab5e5ee2f381e4e54ecdf5cc2bef38
MeanMachineRex <RexRowan@users.noreply.github.com>
2026-08-09 15:41:06 +0000

    get lean

---
+++ lean/QiskitEquiv.lean
@@ -0,0 +1,7 @@
+/-
+  Thin wrapper to expose generated contents as `QiskitEquiv` module.
+
+  Created automatically by the build helper when the generated file is placed
+-/
+
+import QiskitEquiv.Generated
